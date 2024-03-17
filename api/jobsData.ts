d17e8423e4ff1c5d290aed0314b2e7fe9e118227
Wisdom-Daniel <44666155+dew-dr0p@users.noreply.github.com>
2024-03-17 14:52:21 +0100

    modified backend branch

--- api/jobsData.ts
+++
@@ -1,5 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-    res.json({ message: "You've gotten to serverless function" })
-}
--- src/components/MainButton.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <button class="bg-primary text-white p-3 px-8 font-bold rounded-2xl">Have A Look</button>
-</template>
